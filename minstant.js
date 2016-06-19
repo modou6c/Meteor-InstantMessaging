4e3550bb6d47f962e5fe21e930e687be5bd97aae
modou6c <contact.modoucisse@gmail.com>
2016-06-19 17:07:25 +0200

    adding sender avatar

--- minstant.js
+++ minstant.js
@@ -87,7 +87,10 @@ if (Meteor.isClient) {
       // is a good idea to insert data straight from the form
       // (i.e. the user) into the database?? certainly not. 
       // push adds the message to the end of the array
-      msgs.push({text: event.target.chat.value});
+      msgs.push({text: event.target.chat.value, 
+        senderName:Meteor.user().profile.username, 
+        senderAvatar:Meteor.user().profile.avatar
+      });
       // reset the form
       event.target.chat.value = "";
       // put the messages array onto the chat object
